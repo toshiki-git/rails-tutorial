1c6caac8868651cf1c01488ad570e672eca40e17
Toshiki Takakubo <219tosiki@gmail.com>
2023-09-27 15:33:12 +0900

    投稿の更新機能の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,35 @@ class PostsController < ApplicationController
     end
 
     def new
-        @posts = Post.new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+
+        if @post.save
+            redirect_to posts_path
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+        if @post.update(post_params)
+            redirect_to posts_path
+        else
+            render :edit
+        end
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :content)
     end
 end
