8fd8783f7e8ce03eadf7605861f45c776213891e
Toshiki Takakubo <219tosiki@gmail.com>
2023-09-27 15:47:26 +0900

    リファクタリング

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: [:edit, :update, :destroy]
     def index
         @posts = Post.all
     end
@@ -9,7 +10,6 @@ class PostsController < ApplicationController
 
     def create
         @post = Post.new(post_params)
-
         if @post.save
             redirect_to posts_path
         else
@@ -18,11 +18,9 @@ class PostsController < ApplicationController
     end
 
     def edit
-        @post = Post.find(params[:id])
     end
 
     def update
-        @post = Post.find(params[:id])
         if @post.update(post_params)
             redirect_to posts_path
         else
@@ -31,7 +29,6 @@ class PostsController < ApplicationController
     end
 
     def destroy
-        @post = Post.find(params[:id])
         @post.destroy
         redirect_to posts_path
     end
@@ -40,5 +37,9 @@ class PostsController < ApplicationController
 
     def post_params
         params.require(:post).permit(:title, :content)
+    end
+    
+    def set_post
+        @post = Post.find(params[:id])
     end
 end
